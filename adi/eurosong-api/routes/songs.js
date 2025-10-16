5b169a0d2fcdf2944d4bca160cbf044a962a25a6
Collin Van der Vorst <collinvandervorst@Mac.guests.thomasmore.be>
2025-10-16 12:19:52 +0200

    Added songs logic

--- adi/eurosong-api/routes/songs.js
+++ adi/eurosong-api/routes/songs.js
@@ -4,25 +4,55 @@
 const express = require('express');
 const router = express.Router();
 
+const { PrismaClient } = require('../generated/prisma');
+const prisma = new PrismaClient();
+
 // -------------------------
 // [GET] Songs 
 // return array of songs
 // -------------------------
-router.get('/', (req, res) => {
-  // @todo: link to database
-  res.json([]);
+router.get('/', async(req, res) => {
+  const songs = await prisma.songs.findMany({
+    include: {
+      artists: true
+    }
+  });
+  res.json(songs);
 })
 
 // -------------------------
 // [POST] Songs 
 // return id (id kan ook null zijn, niet gelukt )
 // -------------------------
-router.post('/', (req, res) => {
-    // @todo: link to database
-    // req.body -> om data uit een post te halen
-    res.send("Added Songs");
-})
+router.post('/', async(req, res) => {
+    // Get data
+    const artistId = req.body.artist_id;
+    const songName = req.body.name;
 
+    // Bestaat de combinatie? 
+    const combinationCheck = await prisma.songs.findMany({
+      where: {
+        artist_id: artistId,
+        name: songName
+      }
+    })
+
+    if (combinationCheck.length > 0 ) {
+      res.json({
+        "status": "combination already exists"
+      });
+    } else {
+
+      const newSong = await prisma.songs.create({
+            data: {
+              artist_id: artistId,
+              name: songName
+            }
+          });
+        res.send(newSong);
+
+    }
+});
 // -------------------------
 // [DELETE] Songs 
 // return boolean (true or false )
