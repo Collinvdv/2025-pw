e197a4f1254914b8de6b15e262c23ad6c76bbc85
Collin Van der Vorst <collinvandervorst@Mac.guests.thomasmore.be>
2025-11-13 11:58:31 +0100

    Added things

--- adi/eurosong-front/src/pages/Artists.vue
+++ adi/eurosong-front/src/pages/Artists.vue
@@ -2,6 +2,11 @@
     Settings van de pagina
 -->
 <script setup>
+    // Imports
+    import { ref } from 'vue';
+
+    // Data (ref)
+    let artists = ref([])
 </script>
 
 <!--
@@ -11,4 +16,14 @@
     <h1>
         Artists
     </h1>
+
+    <ul v-if="artists.length > 0">
+        <li v-for="artist in artists" :key="artist.artist_id">
+            {{ artist.name }}
+        </li>
+    </ul>
+
+    <p v-if="artists.length == 0">
+        Geen artiesten beschikbaar
+    </p>
 </template>
