5257300cbfcc01e882519fe364faf244afd37fc5
Collin Van der Vorst <collinvandervorst@iPhone.guests.thomasmore.be>
2025-11-19 14:51:15 +0100

    Add songs

--- dsps/eurosong-frontend/src/pages/Voting.vue
+++ dsps/eurosong-frontend/src/pages/Voting.vue
@@ -1,6 +1,25 @@
 <!-- Script -->
 <script setup>
+// Import 
+    import { onMounted, ref } from 'vue';
 
+    // Data 
+    const songs = ref([]);
+
+    // Lifecycle
+    onMounted(() => {
+        // to be used in my list
+        fetchSongs();
+    })
+
+    // methods 
+    const fetchSongs = () => {
+        fetch("http://localhost:3000/songs")
+            .then((res) => res.json())
+            .then((data) => {
+                songs.value = data;
+            })
+    }
 </script>
 
 <!-- Template -->
@@ -8,4 +27,13 @@
     <h1>
         Voting
     </h1>
+
+    <ul>
+        <li v-for="song in songs" :key="song.id">
+            {{ song.artists.name }} - {{  song.name }}
+        </li>
+    </ul>
+
  </template>
+
+ <!--  I WANT TO A LIST OF ALL THE SONGS -->
