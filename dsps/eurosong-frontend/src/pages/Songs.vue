a1d98a5781d981ee3667a805e09554283ee18363
Collin Van der Vorst <collinvandervorst@iPhone.guests.thomasmore.be>
2025-11-19 14:29:30 +0100

    Song + added

--- dsps/eurosong-frontend/src/pages/Songs.vue
+++ dsps/eurosong-frontend/src/pages/Songs.vue
@@ -4,16 +4,54 @@
     import { onMounted, ref } from 'vue';
 
     // Data 
-    const songs = ref([])
+    const songs = ref([]);
+    const artists = ref([]);
+    const newSongName = ref("");
+    const newArtistId =ref(null);
 
     // Lifecycle
     onMounted(() => {
+        // to be used in my list
+        fetchSongs();
+
+        // to be used in my add songs (for the dropdown)
+        fetchArtists();
+    })
+
+    // methods 
+    const addSongs = () => {
+        fetch('http://localhost:3000/songs', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(
+                {
+                    name: newSongName.value,
+                    artist_id: parseInt(newArtistId.value)
+                }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                fetchSongs();
+            })
+    }
+
+    const fetchSongs = () => {
         fetch("http://localhost:3000/songs")
             .then((res) => res.json())
             .then((data) => {
                 songs.value = data;
             })
-    })
+    }
+
+    const fetchArtists = () => {
+        fetch("http://localhost:3000/artists")
+            .then((res) => res.json())
+            .then((data) => {
+                artists.value = data;
+            })
+    }
 </script>
 
 <!-- Template -->
@@ -26,5 +64,20 @@
                 {{ song.artists.name }} - {{  song.name }}
             </li>
         </ul>
+
+        <hr/>
+        <h2>
+            Create new song
+        </h2>
+        <label>Song name</label>
+        <input type="text" v-model="newSongName">
+
+        <label> Artist</label>
+        <select v-model="newArtistId">
+            <option v-for="artist in artists" :key="artist.id" :value="artist.artist_id">{{ artist.name }}</option>
+        </select>
+        <button @click="addSongs()">
+            Add song
+        </button>
     </div>
  </template>
