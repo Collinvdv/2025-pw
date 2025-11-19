e70bafa6f1904736709ed6bb0d33006285e81de4
Collin Van der Vorst <collinvandervorst@iPhone.guests.thomasmore.be>
2025-11-19 15:27:33 +0100

    Added the code

--- dsps/eurosong-frontend/src/pages/Voting.vue
+++ dsps/eurosong-frontend/src/pages/Voting.vue
@@ -1,10 +1,15 @@
 <!-- Script -->
 <script setup>
 // Import 
-    import { onMounted, ref } from 'vue';
-
+    import { onMounted, ref, reactive } from 'vue';
+    import { useRouter } from 'vue-router'
+    const router = useRouter()
     // Data 
     const songs = ref([]);
+    const activeSongIndex = ref(0);
+    let vote2 = ref(false);
+    let vote4 = ref(false);
+    let vote6 = ref(false);
 
     // Lifecycle
     onMounted(() => {
@@ -20,6 +25,43 @@
                 songs.value = data;
             })
     }
+
+    const nextSong = () => {
+        activeSongIndex.value++;
+    }
+
+    const prevSong = () => {
+        activeSongIndex.value--;
+    }
+
+    const vote = (points) => {
+        fetch('http://localhost:3000/voting', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(
+                {
+                    points: points,
+                    song_id: songs.value[activeSongIndex.value].song_id
+                }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (points == 2) {
+                    vote2.value = true
+                } else if (points == 4) {
+                    vote4.value = true
+                } else if (points == 6) {
+                    vote6.value = true
+                }
+
+                if (vote2.value == true && vote4.value == true && vote6.value == true) {
+                    // redirect to ranking
+                    router.push('/ranking');
+                }
+            })
+    }
 </script>
 
 <!-- Template -->
@@ -28,11 +70,25 @@
         Voting
     </h1>
 
-    <ul>
-        <li v-for="song in songs" :key="song.id">
-            {{ song.artists.name }} - {{  song.name }}
-        </li>
-    </ul>
+    <div>
+        <div v-for="(song, index) in songs" :key="song.id" >
+            <div v-if="index == activeSongIndex">
+                {{ song.artists.name }} - {{  song.name }}
+            </div>
+        </div>
+
+        <button @click="prevSong()" :disabled="activeSongIndex <= 0">
+            Prev song
+        </button>
+        <button @click="nextSong()" :disabled="activeSongIndex >= (songs.length - 1)">
+            Next song
+        </button>
+    </div>
+
+    <hr/>
+    <button @click="vote(2)" :disabled="vote2 != false">Vote 2 points</button>
+    <button @click="vote(4)" :disabled="vote4 != false">Vote 4 points</button>
+    <button @click="vote(6)" :disabled="vote6 != false">Vote 6 points</button>
 
  </template>
 
