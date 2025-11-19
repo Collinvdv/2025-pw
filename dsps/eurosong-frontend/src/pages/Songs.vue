e01781a4f4f17843f7f0bc57725f919e003fbeeb
Collin Van der Vorst <collinvandervorst@iPhone.guests.thomasmore.be>
2025-11-19 13:59:14 +0100

    Added song data in vue

--- dsps/eurosong-frontend/src/pages/Songs.vue
+++ dsps/eurosong-frontend/src/pages/Songs.vue
@@ -1,11 +1,40 @@
 <!-- Script -->
 <script setup>
+    // Import 
+    import { onMounted, ref } from 'vue';
 
+    // Data 
+    const songs = ref([
+        {
+            "song_id": 1,
+            "name": "Waterloo",
+            "artist_id": 1,
+            "artists": {
+                "artist_id": 1,
+                "name": "ABBA"
+            }
+        },
+        {
+            "song_id": 2,
+            "name": "Ne partez pas sans moi",
+            "artist_id": 2,
+            "artists": {
+                "artist_id": 2,
+                "name": "Celine Dion"
+            }
+        },
+    ])
 </script>
 
 <!-- Template -->
  <template>
-    <h1>
-        Songs
-    </h1>
+    <div>
+        <h1> Songs overview </h1>
+
+        <ul>
+            <li v-for="song in songs" :key="song.id">
+                {{ song.artists.name }} - {{  song.name }}
+            </li>
+        </ul>
+    </div>
  </template>
