a7dd21438601cea9411e8c5c29e03f6c494b86a6
Collin Van der Vorst <collinvandervorst@iPhone.guests.thomasmore.be>
2025-11-19 14:39:54 +0100

    Ranking page

--- dsps/eurosong-frontend/src/pages/Ranking.vue
+++ dsps/eurosong-frontend/src/pages/Ranking.vue
@@ -1,5 +1,19 @@
 <!-- Script -->
 <script setup>
+    // import
+    import {ref , onMounted} from 'vue'
+
+    // data 
+    const ranking = ref([]);
+
+    // lifecycles
+    onMounted(() => {
+        fetch("http://localhost:3000/ranking")
+            .then((res) => res.json())
+            .then((data) => {
+                ranking.value = data;
+            })
+    })
 
 </script>
 
@@ -8,4 +22,29 @@
     <h1>
         Ranking
     </h1>
+
+    <table border="1">
+        <tr>
+            <th>
+                Artist name
+            </th>
+            <th>
+                Song name
+            </th>
+            <th>
+                Points
+            </th>
+        </tr>
+        <tr v-for="item in ranking" :key="item.song_id">
+            <td>
+                {{ item.artistname }}
+            </td>
+            <td>
+                {{ item.songname }}
+            </td>
+            <td>
+                {{ item.totalpoint }}
+            </td>
+        </tr>
+    </table>
  </template>
---
+++ dsps/eurosong-frontend/src/pages/_example.vue
@@ -0,0 +1,18 @@
+<!-- Script -->
+<script setup>
+    // import
+    import { ref , onMounted} from 'vue'
+
+    // data 
+    
+    // lifecycle
+
+    // methods 
+</script>
+
+<!-- Template -->
+ <template>
+    <h1>
+        Example
+    </h1>
+ </template>
